fbd97c4c8f4848ac853d32710a13c0d02eb1a102
stenioas <stenioas@gmail.com>
2021-06-19 07:01:35 -0300

    chore: some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SideMenu from "./components/SideMenu";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+
+export default function App() {
+	return (
+		<div className="container">
+			<SideMenu />
+			<Header />
+			<Login />
+		</div>
+	);
 }
-
-export default App;
