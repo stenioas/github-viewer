3d84cbfa3f65bd913154232e0401bbd3a9b5731c
stenioas <stenioas@gmail.com>
2021-06-19 04:37:48 -0300

    style(login): border-radius and colors modified

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,15 +3,22 @@ import Form from "../../../src/components/Form";
 
 export default function Login() {
 	const Wrapper = styled.div`
-		height: 100%;
+		position: relative;
+		height: 100vh;
 		width: 100%;
 		display: flex;
 		align-items: center;
 		justify-content: center;
 	`;
+	const Logo = styled.img`
+		top: 8rem;
+		position: absolute;
+		margin: 0 auto;
+	`;
 
 	return (
 		<Wrapper>
+			<Logo src="logo.svg" alt="Logo" />
 			<Form />
 		</Wrapper>
 	);
